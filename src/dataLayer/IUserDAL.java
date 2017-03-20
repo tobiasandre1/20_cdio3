58f47adc06e7dc3bacb9e3ac7302f04fb8aeff47
tobiasandre1 <hoejsgaard.tobias@gmail.com>
2017-03-20 16:28:39 +0100

    Made interface for data layer

--- src/dataLayer/IUserDAL.java
+++ src/dataLayer/IUserDAL.java
@@ -1,5 +1,32 @@
 package dataLayer;
 
+import java.util.List;
+
+import dataTransferObject.UserDTO;
+
 public interface IUserDAL {
+
+	UserDTO getUser(int userId) throws DALException;
+	List<UserDTO> getUserList() throws DALException;
+	void createUser(UserDTO user) throws DALException;
+	void updateUser(UserDTO user) throws DALException;
+	boolean deleteUser(int userId) throws DALException;
+	
+	public class DALException extends Exception {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 7355418246336739229L;
+
+		public DALException(String msg, Throwable e) {
+			super(msg,e);
+		}
+
+		public DALException(String msg) {
+			super(msg);
+		}
+
+	}
 
 }
