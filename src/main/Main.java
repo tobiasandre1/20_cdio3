f418755e3bfa4446daee5c22d0bbdf4f76fae87b
tobiasandre1 <hoejsgaard.tobias@gmail.com>
2017-03-27 15:56:38 +0200

    Added import statements

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,16 @@
 package main;
 
+import presentationLayer.*;
+import functionalityLayer.*;
+import dataLayer.*;
+import dataTransferObject.*;
+
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		DBConnector dbcon = new DBConnector();
+		dbcon.getData();
 	}
 
 }
