2f6f930320ef9aefbe3b9b47a7779ac6d5e72547
tobiasandre1 <hoejsgaard.tobias@gmail.com>
2017-03-28 09:52:00 +0200

    Last interface

---
+++ src/dataLayerInterfaces/RaavareDAO.java
@@ -0,0 +1,12 @@
+package dataLayerInterfaces;
+
+import java.util.List;
+
+import dataTransferObject.RaavareDTO;
+
+public interface RaavareDAO {
+	RaavareDTO getRaavare(int raavareId) throws DALException;
+	List<RaavareDTO> getRaavareList() throws DALException;
+	void createRaavare(RaavareDTO raavare) throws DALException;
+	void updateRaavare(RaavareDTO raavare) throws DALException;
+}
