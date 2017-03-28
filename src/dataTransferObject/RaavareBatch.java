4fe7591be36e2778b61fc7826668dd21c979f784
tobiasandre1 <hoejsgaard.tobias@gmail.com>
2017-03-28 08:41:30 +0200

    Created DTO for RaavareBatch table

---
+++ src/dataTransferObject/RaavareBatch.java
@@ -0,0 +1,40 @@
+package dataTransferObject;
+
+public class RaavareBatch {
+	private int rb_id;
+	private int raavare_id;
+	private int maengde;
+	
+	public RaavareBatch(int rb_id, int raavare_id, int maengde) {
+		this.rb_id = rb_id;
+		this.raavare_id = raavare_id;
+		this.maengde = maengde;
+	}
+
+	public int getRb_id() {
+		return rb_id;
+	}
+
+	public void setRb_id(int rb_id) {
+		this.rb_id = rb_id;
+	}
+
+	public int getRaavare_id() {
+		return raavare_id;
+	}
+
+	public void setRaavare_id(int raavare_id) {
+		this.raavare_id = raavare_id;
+	}
+
+	public int getMaengde() {
+		return maengde;
+	}
+
+	public void setMaengde(int maengde) {
+		this.maengde = maengde;
+	}
+	
+	
+	
+}
