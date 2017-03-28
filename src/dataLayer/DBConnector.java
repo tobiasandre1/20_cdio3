6dae759d1fdaa7e50ca914901656c514721b3efe
tobiasandre1 <hoejsgaard.tobias@gmail.com>
2017-03-28 09:13:24 +0200

    Connector update

--- src/dataLayer/DBConnector.java
+++ src/dataLayer/DBConnector.java
@@ -14,13 +14,17 @@ public class DBConnector {
 	private java.sql.Statement st;
 	private java.sql.ResultSet rs;
 	
-	private String dataBaseName = "20_weightdb";
+	private String DATABASE = "20_weightdb";
+	private String HOST = "localhost";
+	private int PORT = 3306;
+	private String USER = "root";
+	private String PASSWORD = "";
 	
 	public DBConnector(){
 		try{
 			Class.forName("com.mysql.jdbc.Driver");
 			
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/"+dataBaseName, "root", "");
+			con = DriverManager.getConnection("jdbc:mysql://" + HOST +":" + PORT + "/" +DATABASE, USER, PASSWORD);
 			st = con.createStatement();
 		
 		}catch(Exception e){
