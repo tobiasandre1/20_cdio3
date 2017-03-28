4fcd2610ba02b6769ccc4b25ad0317b8951b4b26
tobiasandre1 <hoejsgaard.tobias@gmail.com>
2017-03-28 09:48:10 +0200

    More interfaces added

---
+++ src/dataLayerInterfaces/ProduktBatchDAO.java
@@ -0,0 +1,12 @@
+package dataLayerInterfaces;
+
+import java.util.List;
+
+import dataTransferObject.ProduktBatchDTO;
+
+public interface ProduktBatchDAO {
+	ProduktBatchDTO getProduktBatch(int pbId) throws DALException;
+	List<ProduktBatchDTO> getProduktBatchList() throws DALException;
+	void createProduktBatch(ProduktBatchDTO produktbatch) throws DALException;
+	void updateProduktBatch(ProduktBatchDTO produktbatch) throws DALException;
+}
---
+++ src/dataLayerInterfaces/ProduktBatchKompDAO.java
@@ -0,0 +1,14 @@
+package dataLayerInterfaces;
+
+import java.util.List;
+
+import dataTransferObject.ProduktBatchKomponentDTO;
+
+public interface ProduktBatchKompDAO {
+	ProduktBatchKomponentDTO getProduktBatchKomp(int pbId, int rbId) throws DALException;
+	List<ProduktBatchKomponentDTO> getProduktBatchKompList(int pbId) throws DALException;
+	List<ProduktBatchKomponentDTO> getProduktBatchKompList() throws DALException;
+	void createProduktBatchKomp(ProduktBatchKomponentDTO produktbatchkomponent) throws DALException;
+	void updateProduktBatchKomp(ProduktBatchKomponentDTO produktbatchkomponent) throws DALException;	
+}
+
