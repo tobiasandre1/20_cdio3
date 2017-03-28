e86ff8f189a0d12327dfffde01f520542e9a2be3
tobiasandre1 <hoejsgaard.tobias@gmail.com>
2017-03-28 08:30:32 +0200

    Made DTO for Operatoer table

---
+++ src/dataTransferObject/OperatoerDTO.java
@@ -0,0 +1,49 @@
+package dataTransferObject;
+
+public class OperatoerDTO {
+	private int opr_id;
+	private String opr_navn;
+	private String ini;
+	private String cpr;
+	private String password;
+	
+	public OperatoerDTO(int opr_id, String opr_navn, String ini, String cpr, String password){
+		this.opr_id = opr_id;
+		this.opr_navn = opr_navn;
+		this.ini = ini;
+		this.cpr = cpr;
+		this.password = password;
+	}
+	
+	public int getOpr_id() {
+		return opr_id;
+	}
+	public void setOpr_id(int opr_id) {
+		this.opr_id = opr_id;
+	}
+	public String getOpr_navn() {
+		return opr_navn;
+	}
+	public void setOpr_navn(String opr_navn) {
+		this.opr_navn = opr_navn;
+	}
+	public String getIni() {
+		return ini;
+	}
+	public void setIni(String ini) {
+		this.ini = ini;
+	}
+	public String getCpr() {
+		return cpr;
+	}
+	public void setCpr(String cpr) {
+		this.cpr = cpr;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
